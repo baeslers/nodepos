febec866aced1d5e3230b70e8216919c5949194e
h3xc0ntr0l <h3xc0ntr0l@gmail.com>
2015-05-23 14:58:39 -0400

    commit

--- public/controllers/cardctrl.js
+++ public/controllers/cardctrl.js
@@ -2,5 +2,20 @@ angular.module('cardctrl', ['app']).controller('cardctrl', [
 '$scope',
 'SquareService',
 function($scope, SquareService){
-	
+  	$scope.cart = [{
+    	name: 'Hoss',
+    	price: 7.99
+  	},
+  	{
+    	name: 'Jerk',
+    	price: 6.99
+  	},
+  	{
+    	name: 'Small Coffee',
+    	price: 1.99
+    }];
+  $scope.remove = function(item){
+  	$scope.cart.splice($scope.cart.indexOf(item), $scope.cart.indexOf(item)+1);
+  	console.log($scope.cart);
+  }
 }]);
